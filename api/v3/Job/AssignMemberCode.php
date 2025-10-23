559d187664527a24fbb20f529dc267d6410aa32e
colemanw <coleman@civicrm.org>
2025-10-22 20:29:52 -0400

    Replace reference to deprecated exception class
    The api-specific exception classes have been deprecated and will soon be removed from core.
    They are identical to CRM_Core_Exception.

--- api/v3/Job/AssignMemberCode.php
+++ api/v3/Job/AssignMemberCode.php
@@ -26,7 +26,7 @@ function _civicrm_api3_job_assign_member_code_spec(&$spec)
  *
  * @see civicrm_api3_create_success
  *
- * @throws API_Exception
+ * @throws CRM_Core_Exception
  */
 function civicrm_api3_job_assign_member_code($params)
 {
